fb9270fd211c63d125bea7ec28d88ad37ed7704a
Rimold Rynold <rimoldishere@gmail.com>
2021-05-19 17:07:58 +0530

    Initial commit

---
+++ todoapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('submit',views.submit,name='submit'),
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('list',views.list,name='list'),
+    path('sortdata',views.sortdata,name='sortdata'),
+    path('searchdata',views.searchdata,name='searchdata'),
+    path('submit',views.submit,name='submit'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('update/<int:id>',views.update,name='update'),
+]
